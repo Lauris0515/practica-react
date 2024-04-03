abe2f6493f822b3d89a56ffb67c6b70a97d4fe2e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-03 17:48:20 -0500

    cambios

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -1,12 +1,45 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import basedatosusuarios from "../utils/basedatosusuraios.json"
+import Swal from 'sweetalert2'
 
 export function Formulario(){
 
-    const[verCedula,modificarCedula]=useState("")
-    const[verContraseña,modificarContraseña]=useState("")
+    const[verCedula,guardarCedula]=useState("")
+    const[verContraseña,guardarContraseña]=useState("")
 
-    function procesarFormulario(){
-        
+    // Para Navegar entre componentes debo declarar un variable que almacene el hook
+
+    let enrutador=useNavigate()
+
+    function procesarFormulario(evento){
+        // Que hago si le hacn clic al boton de formulario?
+        evento.preventDefault()
+
+        // buscamos cohincidencias entre lo que escribe el usuario formulario y el jsn de la BD
+
+         let busqueda=basedatosusuarios.find(function(usuario){
+            return(usuario.documento==verCedula)
+         })
+
+         if(busqueda==undefined) {
+       
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+                footer: '<a href="#">Why do I have this issue?</a>'
+              });
+
+         }else {
+
+        //Voy a enrutar otro componente (como lanzo un componente desde otro?)
+         enrutador("/home")
+
+         }
+
+       
     }
 
 
@@ -16,7 +49,7 @@ export function Formulario(){
             <div className="row justify-content-center text-center">
             <div className="col-12 col-md-6">
                 <img src="../../src/assets/img/logo-sura.png" alt="logo" className="img-fluid" />
-                <form className="border rounded p-4">
+                <form className="border rounded p-4" onSubmit={procesarFormulario}>
                     <h2>Controla tu salud</h2>
                     <div class="input-group mb-3 mt-5">
                     <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-vcard"></i></span>  
@@ -25,6 +58,7 @@ export function Formulario(){
                     class="form-control" 
                     placeholder="Numero cedula"
                     id="identificacion"
+                    onChange={function(evento){guardarCedula(evento.target.value)}}
                     />
                   </div>
                   <div class="input-group mb-3">
@@ -34,10 +68,11 @@ export function Formulario(){
                   class="form-control" 
                   placeholder="Contraseña"
                   id="contraseña"
+                  onChange={function(evento){guardarContraseña(evento.target.value)}}
                  />
                 </div>
-                </form>
                 <button type="submit" className="btn btn-primary mt-3 ">Ingresar</button>
+                </form>               
             </div>
             </div>
         </section>
